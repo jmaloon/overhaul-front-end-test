fbc08ba29d5848b152995a9bc857ee6542bc4afe
jmaloon <joshmaloon@gmail.com>
2024-08-11 09:45:24 -0600

    architecting progress component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,29 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="progress-wrapper">
+      <ol className="progress">
+        <li className="progress-step progress-step__past">
+          <span className="visually-hidden">Completed: </span>
+          <span className="progress-step">Step one</span>
+          <span className="progress-label">Your basket</span>
+        </li>
+        <li className="progress-step progress-step__past">
+          <span className="visually-hidden">Completed: </span>
+          <span className="progress-step">Step two</span>
+          <span className="progress-label">Your details</span>
+        </li>
+        <li className="progress-step progress-step__current">
+          <span className="visually-hidden">Completed: </span>
+          <span className="progress-step">Step three</span>
+          <span className="progress-label">Payment</span>
+        </li>
+        <li className="progress-step">
+          <span className="progress-step">Step four</span>
+          <span className="progress-label">Order complete</span>
+        </li>
+      </ol>
+    </div>
   );
 }
 
